ff01b06106eda697f71d4bd980605b7ae3d1575d
kutlukarakoc <karakockutlu@gmail.com>
2023-11-23 21:33:28 +0300

    updated folder structure for pages

---
+++ app/(routes)/(admin)/giris/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'Diriliş Yayınları | Giriş',
+}
+
+export default function Login() {
+  return (
+		<section>
+			login
+		</section>
+	)
+}
---
+++ app/(routes)/(admin)/yonetim-tablosu/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'Diriliş Yayınları | Yönetim Tablosu',
+}
+
+export default function ManagementTable() {
+  return (
+		<section>
+			managament table
+		</section>
+	)
+}
