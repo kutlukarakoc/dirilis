de9648bec6975a88e79bf49d80651e05d63cad35
kutlukarakoc <karakockutlu@gmail.com>
2023-12-06 00:53:49 +0300

    display different ui on login depends on session

--- app/(admin)/giris/page.tsx
+++ app/(admin)/giris/page.tsx
@@ -1,9 +1,13 @@
 import Login from "@/containers/giris"
+import AlreadyLoggedIn from "@/containers/giris/already-loggedin"
+import { getServerSession } from "next-auth"
 
 export const metadata = {
   title: 'Diriliş Yayınları | Giriş',
 }
 
-export default function Page() {
-  return <Login />
+export default async function Page() {
+	const session = await getServerSession()
+	
+  return session ? <AlreadyLoggedIn /> : <Login />
 }
