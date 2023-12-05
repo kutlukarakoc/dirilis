05b1a015d4cf8c47081fa6c1332d62fdd03a68c4
kutlukarakoc <karakockutlu@gmail.com>
2023-12-06 00:56:03 +0300

    completed login page and logic

--- containers/giris/index.tsx
+++ containers/giris/index.tsx
@@ -15,8 +15,17 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import * as z from 'zod'
 import { loginSchema } from "@/lib/schemas/loginSchema"
 import { Button } from "@/components/ui/button"
+import { signIn} from "next-auth/react"
+import { useState } from "react"
+import { useRouter } from 'next/navigation'
+import { Loader2 } from "lucide-react"
 
 const Login = () => {
+	const router = useRouter()
+
+	const [error, setError] = useState<null | string>(null)
+	const [loading, setLoading] = useState(false)
+
 	const form = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
@@ -25,57 +34,72 @@ const Login = () => {
     },
   })
 
-  function onSubmit(values: z.infer<typeof loginSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  async function onSubmit(values: z.infer<typeof loginSchema>) {
+		setLoading(true)
+		const res = await signIn('credentials', {
+			email: values.email,
+			password: values.password,
+			redirect: false,
+		})
+
+		setLoading(false)
+		if(res?.error) return setError(res.error)
+
+		router.replace('/yonetim-tablosu')
   }
 
 	return (
-		<section>
+		<section style={{ height: '100vh' }}>
 			<div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
 				<Logo className="text-primary-600 mx-auto mb-10" />
 
 				<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-				<Form {...form}>
-				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-					<FormField
-						control={form.control}
-						name="email"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Email</FormLabel>
-								<FormControl>
-									<Input placeholder="Email adresiniz" {...field} />
-								</FormControl>
-								<FormDescription>
-									Email adresinizi giriniz.
-								</FormDescription>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<FormField
-						control={form.control}
-						name="password"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Şifre</FormLabel>
-								<FormControl>
-									<Input type="password" placeholder="Şifreniz" {...field} />
-								</FormControl>
-								<FormDescription>
-									Şifrenizi giriniz.
-								</FormDescription>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					
-					<Button type="submit">Giriş Yap</Button>
-				</form>
-			</Form>
+					<Form {...form}>
+						<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+							<FormField
+								control={form.control}
+								name="email"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Email</FormLabel>
+										<FormControl>
+											<Input placeholder="Email adresiniz" {...field} />
+										</FormControl>
+										<FormDescription>
+											Email adresinizi giriniz.
+										</FormDescription>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="password"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Şifre</FormLabel>
+										<FormControl>
+											<Input type="password" placeholder="Şifreniz" {...field} />
+										</FormControl>
+										<FormDescription>
+											Şifrenizi giriniz.
+										</FormDescription>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<Button
+								type="submit"
+								disabled={loading}
+								aria-disabled={loading}
+							>
+								{loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+								Giriş Yap
+							</Button>
+						</form>
+					</Form>
 
+					{error && <p className="mt-8 text-center text-paragraph font-medium text-red-500">{error}</p>}
 				</div>
 			</div>
 		</section>
