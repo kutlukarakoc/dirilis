094db5f23ff5d5e63065be2bc5c635ae283556f2
kutlukarakoc <karakockutlu@gmail.com>
2023-12-06 00:53:26 +0300

    updated admin layout

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,7 +1,3 @@
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
-  return (
-		<main className="container my-20 md:my-32 flex-1 relative">
-			{children}
-		</main>
-	)
+  return <>{children}</>
 }
