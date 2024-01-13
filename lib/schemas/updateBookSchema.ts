119cc5ef78336a83de0c6b87fd2a61f337143248
kutlukarakoc <karakockutlu@gmail.com>
2024-01-13 15:32:44 +0300

    added schema to update book

--- lib/schemas/updateBookSchema.ts
+++ lib/schemas/updateBookSchema.ts
@@ -1,23 +1,41 @@
-import * as z from "zod"
+import * as z from 'zod'
+
+const imageUrlRegex = /^https:\/\/i\.ibb\.co\/.*\.webp$/
 
 export const updateBookSchema = z.object({
-	price: z.number({
-		invalid_type_error: 'Sadece rakam girilebilir.'
-	}),
+  price: z.coerce.number({
+    invalid_type_error: 'Sadece rakam girilebilir.',
+    required_error: 'Fiyat alanı boş bırakılamaz.',
+  }),
 
-	isbn: z.string(),
+  isbn: z.string({
+    required_error: 'ISBN alanı boş bırakılamaz.',
+  }),
 
-	lastNo: z.number({
-		invalid_type_error: 'Sadece rakam girilebilir.'
-	}),
+  lastNo: z.coerce.number({
+    invalid_type_error: 'Sadece rakam girilebilir.',
+    required_error: 'Son Baskı Numarası alanı boş bırakılamaz.',
+  }),
 
-	firstDate: z.number({
-		invalid_type_error: 'Sadece rakam girilebilir.'
-	}),
+  firstDate: z.coerce.number({
+    invalid_type_error: 'Sadece rakam girilebilir.',
+    required_error: 'İlk Baskı Yılı alanı boş bırakılamaz.',
+  }),
 
-	lastDate: z.string(),
+  lastDate: z.string({
+    required_error: 'Son Baskı Tarihi alanı boş bırakılamaz.',
+  }),
 
-	pages: z.number({
-		invalid_type_error: 'Sadece rakam girilebilir.'
-	})
+  pages: z.coerce.number({
+    invalid_type_error: 'Sadece rakam girilebilir.',
+    required_error: 'Sayfa Sayısı alanı boş bırakılamaz.',
+  }),
+
+  imageUrl: z
+    .string({
+      required_error: 'Görsel linki alanı boş bırakılamaz',
+    })
+    .regex(imageUrlRegex, {
+      message: 'Link https://i.ibb.co/ ile başlamalı ve .webp ile bitmelidir.',
+    }),
 })
