c3d42ea1cf29d8c4bf6de334210f7c50ffbf95e8
kutlukarakoc <karakockutlu@gmail.com>
2024-01-13 15:33:04 +0300

    added type for book management

--- types/bookManagament.ts
+++ types/bookManagament.ts
@@ -1,10 +1,11 @@
-import { Publish } from "./books"
+import { Publish } from './books'
 
 export interface BookManagement {
-	title: string
-	pages: number
-	price: number
-	publish: Publish
-	isbn: string
-	_id: string
+  title: string
+  pages: number
+  price: number
+  publish: Publish
+  isbn: string
+  imageUrl: string
+  _id: string
 }
