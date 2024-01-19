f959756e6d491ecf14df793bccbaa80d789e5cfb
kutlukarakoc <karakockutlu@gmail.com>
2024-01-19 19:38:34 +0300

    secret key debug

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -5,6 +5,7 @@ export default function AdminLayout({
 }: {
   children: React.ReactNode
 }) {
+	console.log('process.env.NEXTAUTH_SECRET', process.env.NEXTAUTH_SECRET)	
   return (
     <>
       {children}
