ab2268c03d6ba604819117e7293be836734f3936
kutlukarakoc <karakockutlu@gmail.com>
2024-02-06 20:58:11 +0300

    updated login schema

--- lib/schemas/loginSchema.ts
+++ lib/schemas/loginSchema.ts
@@ -2,18 +2,15 @@ import * as z from 'zod'
 
 export const loginSchema = z.object({
   email: z
-    .string({
-      required_error: 'Email alanı boş bırakılamaz.',
+    .string()
+    .min(1, {
+      message: 'Email alanı boş bırakılamaz.',
     })
     .email({
       message: 'Geçerli bir email giriniz.',
     }),
 
-  password: z
-    .string({
-      required_error: 'Şifre alanı boş bırakılamaz.',
-    })
-    .min(1, {
-      message: 'Şifre alanı boş bırakılamaz.',
-    }),
+  password: z.string().min(1, {
+    message: 'Şifre alanı boş bırakılamaz.',
+  }),
 })
