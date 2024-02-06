00d49979bfd9f796c72872e9b41deadd4c7c2a14
kutlukarakoc <karakockutlu@gmail.com>
2024-02-06 20:54:45 +0300

    added required messages to update book form

--- lib/schemas/updateBookSchema.ts
+++ lib/schemas/updateBookSchema.ts
@@ -1,33 +1,45 @@
 import * as z from 'zod'
 
 export const updateBookSchema = z.object({
-  price: z.coerce.number({
-    invalid_type_error: 'Sadece rakam girilebilir.',
-    required_error: 'Fiyat alanı boş bırakılamaz.',
+  price: z
+    .number({
+      invalid_type_error: 'Sadece rakam girilebilir.',
+    })
+    .min(1, {
+      message: 'Fiyat alanı boş bırakılamaz.',
+    }),
+
+  isbn: z.string().min(1, {
+    message: 'ISBN alanı boş bırakılamaz.',
   }),
 
-  isbn: z.string({
-    required_error: 'ISBN alanı boş bırakılamaz.',
+  lastNo: z
+    .number({
+      invalid_type_error: 'Sadece rakam girilebilir.',
+    })
+    .min(1, {
+      message: 'Son Baskı Numarası alanı boş bırakılamaz.',
+    }),
+
+  firstDate: z
+    .number({
+      invalid_type_error: 'Sadece rakam girilebilir.',
+    })
+    .min(1, {
+      message: 'İlk Baskı Yılı alanı boş bırakılamaz.',
+    }),
+
+  lastDate: z.string().min(1, {
+    message: 'Son Baskı Tarihi alanı boş bırakılamaz.',
   }),
 
-  lastNo: z.coerce.number({
-    invalid_type_error: 'Sadece rakam girilebilir.',
-    required_error: 'Son Baskı Numarası alanı boş bırakılamaz.',
-  }),
-
-  firstDate: z.coerce.number({
-    invalid_type_error: 'Sadece rakam girilebilir.',
-    required_error: 'İlk Baskı Yılı alanı boş bırakılamaz.',
-  }),
-
-  lastDate: z.string({
-    required_error: 'Son Baskı Tarihi alanı boş bırakılamaz.',
-  }),
-
-  pages: z.coerce.number({
-    invalid_type_error: 'Sadece rakam girilebilir.',
-    required_error: 'Sayfa Sayısı alanı boş bırakılamaz.',
-  }),
+  pages: z
+    .number({
+      invalid_type_error: 'Sadece rakam girilebilir.',
+    })
+    .min(1, {
+      message: 'Sayfa Sayısı alanı boş bırakılamaz.',
+    }),
 
   imageUrl: z
     .string({
