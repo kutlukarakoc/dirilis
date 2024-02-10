943d93ab71987cac49ddc4d90ed7e63a19ffb889
kutlukarakoc <karakockutlu@gmail.com>
2024-02-10 17:46:51 +0300

    updated the way of handling contents with new type and text keys

--- containers/kitap/tabs/contents/contentTexts.tsx
+++ containers/kitap/tabs/contents/contentTexts.tsx
@@ -3,24 +3,18 @@ import { Content } from '@/types/books'
 const ContentTexts = ({ content }: { content: Content }) => {
   return (
     <div>
-      {content.heading && (
+      {content.type === 'title' ? (
         <div className="mb-1 ml-8 font-semibold text-primary-700">
-          {content.heading}
+          {content.text}
         </div>
-      )}
-      {'subtitle' in content && content.subtitle && (
+      ) : (
         <div className="mb-1 flex text-paragraph-mobile md:text-paragraph-tablet xl:text-paragraph">
           <span className="inline-block w-8 min-w-[32px] text-primary-700">
-            {content.subtitle.split(' ').slice(0, 1)}
+            {content.text.split(' ').slice(0, 1)}
           </span>
           <span className="text-black-500">
-            {content.subtitle.split(' ').slice(1).join(' ')}
+            {content.text.split(' ').slice(1).join(' ')}
           </span>
-        </div>
-      )}
-      {content.title && (
-        <div className="mb-1 ml-8 font-semibold text-primary-700">
-          {content.title}
         </div>
       )}
     </div>
