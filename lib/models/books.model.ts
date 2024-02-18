3fd922bd2008c1f82f63d6202b4f5ace7b910773
kutlukarakoc <karakockutlu@gmail.com>
2024-02-18 17:46:07 +0300

    ignored versionKey from newly added documents

--- lib/models/books.model.ts
+++ lib/models/books.model.ts
@@ -1,31 +1,35 @@
 import mongoose from 'mongoose'
 
-const bookSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  price: { type: Number, required: true },
-  pages: { type: Number, required: true },
-  thickness: { type: String, required: true },
-  dimension: { type: String, required: true },
-  publish: {
-    lastNo: { type: Number, required: true },
-    firstDate: { type: Number, required: true },
-    lastDate: { type: String, required: true },
-  },
-  isbn: { type: String, required: true },
-  category: {
-    name: { type: String, required: true },
-    id: { type: String, required: true },
-  },
-  imageUrl: { type: String, required: true },
-  contents: [
-    {
-      subtitle: { type: String, required: false },
-      title: { type: String, required: false },
-      heading: { type: String, required: false },
+const bookSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    pages: { type: Number, required: true },
+    thickness: { type: String, required: true },
+    dimension: { type: String, required: true },
+    publish: {
+      lastNo: { type: Number, required: true },
+      firstDate: { type: Number, required: true },
+      lastDate: { type: String, required: true },
     },
-  ],
-  summary: { type: String, required: true },
-})
+    isbn: { type: String, required: true },
+    category: {
+      name: { type: String, required: true },
+      id: { type: String, required: true },
+    },
+    imageUrl: { type: String, required: true },
+    contents: [
+      {
+        type: { type: String, required: false },
+        text: { type: String, required: false },
+      },
+    ],
+    summary: { type: String, required: true },
+  },
+  {
+    versionKey: false,
+  },
+)
 
 const Book = mongoose.models.Book || mongoose.model('Book', bookSchema)
 
