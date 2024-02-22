31d1d47e0219b12d8c7ce17e7a9aeea9b0446aeb
kutlukarakoc <karakockutlu@gmail.com>
2024-02-22 20:57:04 +0300

    updated book detail page loading skeleton

--- app/(user)/kitap/[slug]/loading.tsx
+++ app/(user)/kitap/[slug]/loading.tsx
@@ -3,12 +3,14 @@ import { Skeleton } from '@/components/ui/skeleton'
 export default function Loading() {
   return (
     <section>
-      <div className="flex flex-col items-center justify-between md:flex-row md:items-start">
-        <Skeleton className="mb-14 h-96 max-w-[362px] md:mb-0 md:w-4/12 lg:h-[539px]" />
-        <div className="w-full md:w-7/12">
+      <div className="grid grid-cols-1 lg:grid-cols-2">
+        <div className="mb-14 flex h-[280px] md:h-[450px] lg:h-[575px] items-center justify-center rounded-md bg-primary-50 py-5 lg:mb-0 lg:max-w-[80%]">
+          <Skeleton className="h-full w-[160px] md:w-full max-w-[305px] max-h-[450px]" />
+        </div>
+        <div className="w-full">
           <Skeleton className="mx-auto mb-10 h-8 w-64 md:mx-0 lg:mb-16 xl:h-12 xl:w-96"></Skeleton>
           <Skeleton className="mb-14 h-7 w-32 md:w-28 lg:mb-20 xl:h-9 xl:w-32"></Skeleton>
-          <div className="flex justify-between md:w-11/12">
+          <div className="flex justify-between">
             <Skeleton className="h-7 w-20 xl:h-8 xl:w-32" />
             <Skeleton className="h-7 w-20 xl:h-8 xl:w-28" />
             <Skeleton className="h-7 w-20 xl:h-8 xl:w-24" />
