967608d16a29e39ff2da25ecae2e275b44b074df
kutlukarakoc <karakockutlu@gmail.com>
2025-01-04 14:03:35 +0300

    feat: updated set book prices

--- constants/booksSet.ts
+++ constants/booksSet.ts
@@ -3,17 +3,17 @@ export const booksSet = [
     id: '6547b0ac59301c5b2c1e0d52takim1',
     title:
       'TAKIM 1 (57 lik Set, Hatıralar I ve Hatıralar II ile Genç Müslümana Öğütler hariç)',
-    price: 4580,
+    price: 6780,
   },
   {
     id: '6547b0ac59301c5b2c1e0d52takim2',
     title:
       'TAKIM 2 (60 lik Set, Hatıralar I ve Hatıralar II ile Genç Müslümana Öğütler dahil)',
-    price: 5140,
+    price: 7595,
   },
   {
     id: '6547b0ac59301c5b2c1e0d52takim3',
     title: 'TAKIM 3 (51 lik Set, Gün Doğmadan dahil, Şiirler 1-9 hariç)',
-    price: 4650,
+    price: 6900,
   },
 ]
